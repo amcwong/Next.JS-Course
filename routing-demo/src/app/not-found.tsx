02501968882bc4161d480654cc3713f8bea55b0b
amcwong <andrew.wong8@icloud.com>
2024-07-05 09:40:27 -0400

    not-found.tsx and notFound() function implementation | Tut 10

---
+++ routing-demo/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <>
+        <h2>Page not found</h2>
+        <p>This route does not exist :0</p>
+        </>
+    )
+}
--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,13 @@
+import { notFound } from "../../../../../../node_modules/next/navigation";
+
 export default function ReviewDetail({
   params,
 }: {
   params: { productId: string; reviewId: string };
 }) {
+    if(parseInt(params.reviewId) > 1000) {
+        return notFound();
+    }
   return (
     <h1>
       Review {params.reviewId} for product {params.productId}
