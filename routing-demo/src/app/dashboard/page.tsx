34a59e5baace08aca8a51566177b898b4d4b92c1
amcwong <andrew.wong8@icloud.com>
2024-07-07 23:16:38 -0400

    File Colocation
    // key takeaway project files cane be safely colocated inside route segments
    // rendering line or barchart here can be added to the default export
    
    These can be added to a components folder in some cases.

---
+++ routing-demo/src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+function BarChart() {
+    return <h1>Bar Chart</h1>
+}
+
+export default function Dashboard() {
+    return <h1>Dashboard</h1>
+}
+
+// key takeaway project files cane be safely colocated inside route segments
+// rendering line or barchart here can be added to the default export
