183a661be12687b6f74f574639f42f84d26069e7
amcwong <andrew.wong8@icloud.com>
2024-07-08 17:57:32 -0400

    Link Component Navigation 19

--- routing-demo/src/app/products/page.tsx
+++ routing-demo/src/app/products/page.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
+
 export default function ProductList() {
+  const productId = 100;
   return (
     <>
-      <h1>Product 1</h1>
-      <h1>Product 2</h1>
-      <h1>Product 3</h1>
+      <Link href="/products/1">Product 1</Link>
+      <Link href="/products/2">Product 2</Link>
+      <Link href="/products/3">Product 3</Link>
+      <Link href={`/products/${productId}`}>Product {productId}</Link>
+      <Link href="/">Home</Link>
     </>
   );
 }
