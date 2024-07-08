0891c3ceeb6bbc5a3af4ea27e44d737a33cff525
amcwong <andrew.wong8@icloud.com>
2024-07-08 17:34:39 -0400

    Routing Meta Data.
    Next's Metadata api allows us to define metadata for each page to ensure accurate and relevant information is displayed when pages are shared or indexed.
    
    Configuring meta data: export static metadata object or export a dynamic generateMetadata funciton.
    
    Metadata Rules:
    Both layout and page files can export metadata. If it is defined in a layout it applies to all pages in that layout. However, if defined in a page it applies only to that page. Metadata is read in order from the root level down to the final page level.
    
    When there is meta data in multiple plages for the same route they get combined. The deepest one takes priority if any merge conflicts.
    
    HOWEVER, page metadata will replace layout metadata if they have same properties.

--- routing-demo/src/app/about/page.tsx
+++ routing-demo/src/app/about/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "About Me",
+};
+
 export default function AboutMe() {
-    return <h1>About</h1>;
-  }
+  return <h1>About</h1>;
+}
--- routing-demo/src/app/products/[productId]/page.tsx
+++ routing-demo/src/app/products/[productId]/page.tsx
@@ -1,7 +1,28 @@
-export default function ProductDetails({ params }: { params: {productId: string}}) {
+import { Metadata } from "next";
+
+// or just this
+// export const generateMetadata = (params: { productId: string }): Metadata => {
+//   return { title: `Product ${params.productId}` };
+// };
+
+export const generateMetadata = async ({ params }: Props): Metadata => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iPhone ${params.productId}`);
+    }, 1000);
+  });
+  return { title: `Product ${title}` };
+};
+
+type Props = { params: { productId: string } };
+
+export default function ProductDetails({
+  params,
+}: {
+  params: { productId: string };
+}) {
   return <h1>Details about product {params.productId}</h1>;
 }
 
 // Every page in the app router recieves route parameters as a props.
 // Here we destructure it as params.
-
