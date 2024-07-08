c1c86e39d4d202d58ded9a0bd5868f32517f8129
amcwong <andrew.wong8@icloud.com>
2024-07-08 17:36:04 -0400

    Generating dynamic meta data with promises

--- routing-demo/src/app/products/[productId]/page.tsx
+++ routing-demo/src/app/products/[productId]/page.tsx
@@ -5,13 +5,14 @@ import { Metadata } from "next";
 //   return { title: `Product ${params.productId}` };
 // };
 
-export const generateMetadata = async ({ params }: Props): Metadata => {
+// Generating dynamic meta data with promises
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
   const title = await new Promise((resolve) => {
     setTimeout(() => {
       resolve(`iPhone ${params.productId}`);
     }, 1000);
   });
-  return { title: `Product ${title}` };
+  return Promise.resolve({ title: `Product ${title}` });
 };
 
 type Props = { params: { productId: string } };
