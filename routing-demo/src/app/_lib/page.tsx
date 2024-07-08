529e43ed5c8767d7aac673bbe4ce71088690de49
amcwong <andrew.wong8@icloud.com>
2024-07-07 23:20:09 -0400

    // To include an underscore in urls you can prefix folder name with %5F which is URL encoded form of an underscore

--- routing-demo/src/app/_lib/page.tsx
+++ routing-demo/src/app/_lib/page.tsx
@@ -1,3 +1,4 @@
 export default function PrivateRoute(){
     return <h1>This is a private folder, you cannot view this in browser!</h1>
 }
+// To include an underscore in urls you can prefix folder name with %5F which is URL encoded form of an underscore
